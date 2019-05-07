d60eb3b5838f8c7ee7a9dc6b573ab905e2b8d6ff
Mark Ramirez <markaramirez8@gmail.com>
2019-05-06 21:20:27 -0500

    fetching from s3 bucket

--- poemsradio/src/App.js
+++ poemsradio/src/App.js
@@ -2,30 +2,52 @@ import React from 'react';
 import MusicPlayer from 'react-responsive-music-player';
 import './App.css';
 import Navbar from './Navbar.js'
+import $ from 'jquery';
 
 function App() {
-  const playlist = [
-    {
-      "url": "../clairo.mp3",
-      "cover": "../musicnote.png",
-      "title": "Hot Cheetos instrumental",
-      "artist": [
-        "Clairo"
-      ]
-    },
-    {
-      "url": "src/rejjie.mp3",
-      "cover": "src/musicnote.png",
-      "title": "Oh No instrumental",
-      "artist": [
-        "Rejjie Snow"
-      ]
+
+  var jtext;
+  var url2 = "https://s3.amazonaws.com/robot-poetry-test-bucket/playlist.json";
+  $.ajax({
+    async: false,
+    type: 'POST',
+    dataType: 'jsonp',
+    url: 'https://s3.amazonaws.com/robot-poetry-test-bucket/playlist.json',
+    success: function(data) {
+      console.log("here first")
+      console.log(data)
+      jtext=data;
     }
-  ]
+  });
+
+  $.ajax({
+    async: false,
+    type: 'GET',
+    url: 'https://cors-anywhere.herokuapp.com/' + 'https://s3.amazonaws.com/robot-poetry-test-bucket/playlist.json',
+    success: function(data) {
+      console.log("here two")
+      console.log(data)
+      jtext=data;
+    }
+  });
+
+  $.getJSON(url2, function(result){
+    console.log("here three")
+    console.log(result)
+  });
+
+  console.log("here is the json data:")
+  console.log(jtext)
+  var playlist = jtext;
+
   return (
     <div className="App">
       <Navbar />
-      <MusicPlayer playlist={playlist}/>
+      <div className="poemblurb">
+        <h1>Listen to poems read to you by a robot.</h1>
+        <br/>
+      </div>
+      <div className="Music-player"><MusicPlayer playlist={playlist}/></div>    
     </div>
   );
 }
--- poemsradio/src/Navbar.js
+++ poemsradio/src/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import './Navbar.css'
+import './Navbar.css';
+import prLogo from './pr_logo.png';
+import Image from 'react-bootstrap/Image';
 import {
   Collapse,
   Navbar,
@@ -31,16 +33,16 @@ export default class Example extends React.Component {
     return (
       <div>
         <Navbar style={{backgroundColor: '#808080'}} light expand="md">
-          <NavbarBrand href="/">Poems Radio</NavbarBrand>
+          <NavbarBrand href="/">
+            <Image src= {prLogo} alt="pr_logo" style={{width:50}} />
+          </NavbarBrand>
+          <NavbarBrand href="/">poems radio</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/">About</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-              </NavItem>
+                <NavItem>
+                    <NavLink href="https://github.com/markaramirez/poems-radio">GitHub</NavLink>
+                </NavItem>
             </Nav>
           </Collapse>
         </Navbar>
