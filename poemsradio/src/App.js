bb8ba5cec091f51a793bbc1b72470632f6340e65
Mark Ramirez <markaramirez8@gmail.com>
2019-05-07 19:02:14 -0500

    made site look better

--- poemsradio/src/App.js
+++ poemsradio/src/App.js
@@ -3,51 +3,37 @@ import MusicPlayer from 'react-responsive-music-player';
 import './App.css';
 import Navbar from './Navbar.js'
 import $ from 'jquery';
+import { Jumbotron} from 'reactstrap';
 
 function App() {
 
   var jtext;
-  var url2 = "https://s3.amazonaws.com/robot-poetry-test-bucket/playlist.json";
-  $.ajax({
-    async: false,
-    type: 'POST',
-    dataType: 'jsonp',
-    url: 'https://s3.amazonaws.com/robot-poetry-test-bucket/playlist.json',
-    success: function(data) {
-      console.log("here first")
-      console.log(data)
-      jtext=data;
-    }
-  });
 
   $.ajax({
     async: false,
     type: 'GET',
     url: 'https://cors-anywhere.herokuapp.com/' + 'https://s3.amazonaws.com/robot-poetry-test-bucket/playlist.json',
     success: function(data) {
-      console.log("here two")
-      console.log(data)
       jtext=data;
     }
   });
 
-  $.getJSON(url2, function(result){
-    console.log("here three")
-    console.log(result)
-  });
-
-  console.log("here is the json data:")
-  console.log(jtext)
   var playlist = jtext;
 
   return (
     <div className="App">
       <Navbar />
       <div className="poemblurb">
-        <h1>Listen to poems read to you by a robot.</h1>
+          <h1>Listen to poems read to you by a robot.</h1>
         <br/>
       </div>
-      <div className="Music-player"><MusicPlayer playlist={playlist}/></div>    
+      <Jumbotron>
+        <div className="Music-player"><MusicPlayer playlist={playlist} /></div>
+      </Jumbotron>
+      <Jumbotron>
+        <p>The poems read here are all public domain and are being read to you by <a href="https://aws.amazon.com/polly/">Amazon Polly.</a></p>
+        <p>This web app was built with the help of <a href="https://www.npmjs.com/package/react-responsive-music-player">this music player.</a></p>
+      </Jumbotron>
     </div>
   );
 }
